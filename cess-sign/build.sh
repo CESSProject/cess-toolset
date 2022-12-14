36300c45b2d21d80b5a05a0344347713e83d43e0
AstaFrode <frode@cess.one>
2022-12-14 18:03:18 +0800

    use go mod

--- cess-sign/build.sh
+++ cess-sign/build.sh
@@ -4,9 +4,14 @@ export CGO_ENABLED=0
 export GOOS=linux
 export GOARCH=amd64
 
-rm cess-sign_linux 
+if [[ -f cess-sign_linux ]];then
+    rm -fe cess-sign_linux
+fi
+
 go build -o cess-sign_linux main.go
 
 export GOOS=windows
-rm cess-sign_win.exe
+if [[ -f cess-sign_win.exe ]];then
+    rm -fe cess-sign_win.exe
+fi
 go build -o cess-sign_win.exe main.go
---
+++ cess-sign/go.mod
@@ -0,0 +1,8 @@
+module cess-sign
+
+go 1.19
+
+require (
+	github.com/CESSProject/go-keyring v0.0.0-20220614131247-ee3a8da30fde // indirect
+	github.com/centrifuge/go-substrate-rpc-client/v4 v4.0.10 // indirect
+)
