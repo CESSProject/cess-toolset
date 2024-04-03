73e78322ed270d1babf9e77a1f7b1a3b9685369a
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2024-04-03 16:34:23 +0800

    Add test against a dev node in github action workflow (#8)
    Running test against a `cess-node --dev` in Github CI workflow

--- substrate-rpc-tester/src/test/integration_test.ts
+++ substrate-rpc-tester/src/test/integration_test.ts
@@ -10,10 +10,10 @@ import type { AppConfig } from "../types.ts";
 const SCRIPT_PATH = fromFileUrl(import.meta.resolve("./test_cfg.jsonc"));
 
 Deno.test({
-  name: "Running localhost.jsonc script once should work",
+  name: "Running test_cfg.jsonc script once should work",
   ignore: !rpcEndPoint || rpcEndPoint.length === 0,
   async fn(t) {
-    await t.step("Running localhost.jsonc against cess-node should work", async () => {
+    await t.step("Running test_cfg.jsonc against cess-node should work", async () => {
       console.log(SCRIPT_PATH);
       const configTxt = await Deno.readTextFile(SCRIPT_PATH);
       const config = jsonc.parse(configTxt);
@@ -25,6 +25,8 @@ Deno.test({
 
       const appConfig = config as unknown as AppConfig;
       appConfig.connections = 1;
+      // Replace the config RPC endpoint with the one from env
+      appConfig.endPoint = rpcEndPoint as string;
 
       const substrateRpcTester = new SubstrateRpcTester(appConfig, { verbose: true });
       await substrateRpcTester.initialize();
